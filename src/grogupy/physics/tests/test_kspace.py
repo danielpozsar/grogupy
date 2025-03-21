40311da5dec8641c74d8d74be15ef04cf7911905
Daniel Pozsar <danielpozsar@student.elte.hu>
2025-03-21 13:13:09 +0100

    adding tests

--- src/grogupy/physics/tests/test_kspace.py
+++ src/grogupy/physics/tests/test_kspace.py
@@ -17,6 +17,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+import numpy as np
+import plotly.graph_objects as go
 import pytest
 
 from grogupy.physics import Kspace
@@ -25,8 +27,73 @@ pytestmark = [pytest.mark.physics]
 
 
 class TestKspace:
-    def test_(self):
+    @pytest.mark.parametrize(
+        "kset", [[1, 1, 1], [10, 10, 10], [1, 10, 1], [1, 1, 10], [10, 1, 1]]
+    )
+    def test_kset_and_NK(self, kset):
+        k = Kspace(kset)
+        assert k.NK == np.prod(kset)
+        assert len(k.kpoints) == np.prod(kset)
+        assert len(k.weights) == np.prod(kset)
+        k.kset = [100, 100, 100]
+        assert k.NK == np.prod([100, 100, 100])
+        assert len(k.kpoints) == np.prod([100, 100, 100])
+        assert len(k.weights) == np.prod([100, 100, 100])
+
+    @pytest.mark.parametrize(
+        "kset", [[1, 1, 1], [10, 10, 10], [1, 10, 1], [1, 1, 10], [10, 1, 1]]
+    )
+    def test_kpoints(self, kset):
+        k = Kspace(kset)
         raise Exception("Not implemented test!")
+
+    @pytest.mark.parametrize(
+        "kset", [[1, 1, 1], [10, 10, 10], [1, 10, 1], [1, 1, 10], [10, 1, 1]]
+    )
+    def test_weights(self, kset):
+        k = Kspace(kset)
+        assert np.allclose(k.weights, 1 / np.prod(kset))
+        k.kset = [100, 100, 100]
+        assert np.allclose(k.weights, 1 / np.prod([100, 100, 100]))
+
+    def test_equality(self):
+        k = Kspace([10, 10, 10])
+        k2 = k.copy()
+        assert k == k2
+
+        k2.times = 1
+        assert k != k2
+        k2.times = k.times
+        assert k == k2
+
+        k2.kset = [10, 20, 30]
+        assert k != k2
+        k2.kset = k.kset
+        assert k == k2
+
+    def test_copy(self):
+        k = Kspace([10, 10, 10])
+        k2 = k.copy()
+        assert k == k2
+        k2.kset = [1, 1, 1]
+        assert k != k2
+
+    def test_plot(self):
+        k = Kspace([10, 10, 10])
+        with pytest.raises(Exception):
+            k.plot()
+        import grogupy.viz
+
+        assert isinstance(k.plot(), go.Figure)
+
+    def test_getstate_setstate(self):
+        k = Kspace([10, 10, 10])
+        state = k.__getstate__()
+        assert isinstance(state, dict)
+
+        k2 = object.__new__(Kspace)
+        k2.__setstate__(state)
+        assert k == k2
 
 
 if __name__ == "__main__":
