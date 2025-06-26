ca45b08f90e08480828ae85a846cfb4db8d96842
Daniel Pozsar <danielpozsar@student.elte.hu>
2025-06-26 16:18:34 +0200

    release

--- src/grogupy/__init__.py
+++ src/grogupy/__init__.py
@@ -52,7 +52,7 @@ The ``CONFIG`` instance contains the configuration of grogupy.
 """
 
 __all__ = []
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 version = __version__
 
 # this is for version numbering
