0a65bfa67a471d89442974dc902ffb33b937c5e9
Daniel Pozsar <danielpozsar@student.elte.hu>
2025-10-15 00:03:12 +0100

    updated tests

--- src/grogupy/viz/test/test_viz.py
+++ src/grogupy/viz/test/test_viz.py
@@ -21,8 +21,19 @@
 import plotly.graph_objs as go
 import pytest
 
-import grogupy
-from grogupy.viz import *
+from grogupy.io import load_Builder
+from grogupy.viz import (
+    plot_1D_convergence,
+    plot_contour,
+    plot_DM_distance,
+    plot_DMI,
+    plot_J_S_distance,
+    plot_Jiso_distance,
+    plot_kspace,
+    plot_magnetic_entities,
+    plot_onsite_anisotropy,
+    plot_pairs,
+)
 
 pytestmark = [pytest.mark.viz, pytest.mark.need_benchmark_data]
 
@@ -35,11 +46,124 @@ class TestPlots:
             "./benchmarks/test_builder_2.pkl",
         ],
     )
-    def test_contour(self, path):
-        setup = grogupy.load(path)
-        fig = plot_contour(setup.contour)
+    @pytest.mark.parametrize("marker_size", [1, 10])
+    @pytest.mark.parametrize("marker_opacity", [0.1, 0.5])
+    @pytest.mark.parametrize("width", [500, 1000])
+    @pytest.mark.parametrize("height", [500, 1000])
+    @pytest.mark.parametrize("title", [None, "title"])
+    @pytest.mark.parametrize("legend", [True, False])
+    def test_contour(
+        self, path, marker_size, marker_opacity, width, height, title, legend
+    ):
+        setup = load_Builder(path)
+        if setup.contour is not None:
+            fig = plot_contour(
+                setup.contour,
+                marker_size=marker_size,
+                marker_opacity=marker_opacity,
+                width=width,
+                height=height,
+                title=title,
+                legend=legend,
+            )
+            assert isinstance(fig, go.Figure)
+        else:
+            assert False
+
+    @pytest.mark.parametrize(
+        "path",
+        [
+            "./benchmarks/test_builder.pkl",
+            "./benchmarks/test_builder_2.pkl",
+        ],
+    )
+    @pytest.mark.parametrize("marker_size", [1, 10])
+    @pytest.mark.parametrize("marker_opacity", [0.1, 0.5])
+    @pytest.mark.parametrize("colorscale", ["Viridis", "Plasma"])
+    @pytest.mark.parametrize("width", [500, 1000])
+    @pytest.mark.parametrize("height", [500, 1000])
+    @pytest.mark.parametrize("title", [None, "title"])
+    @pytest.mark.parametrize("legend", [True, False])
+    def test_kspace(
+        self,
+        path,
+        marker_size,
+        marker_opacity,
+        colorscale,
+        width,
+        height,
+        title,
+        legend,
+    ):
+        setup = load_Builder(path)
+        if setup.kspace is not None:
+            fig = plot_kspace(
+                setup.kspace,
+                marker_size=marker_size,
+                marker_opacity=marker_opacity,
+                colorscale=colorscale,
+                width=width,
+                height=height,
+                title=title,
+                legend=legend,
+            )
+            assert isinstance(fig, go.Figure)
+        else:
+            assert False
+
+    @pytest.mark.parametrize(
+        "path",
+        [
+            "./benchmarks/test_builder.pkl",
+            "./benchmarks/test_builder_2.pkl",
+        ],
+    )
+    @pytest.mark.parametrize("tags", [None, ["a", "b", "c"]])
+    @pytest.mark.parametrize("colors", [None, ["blue", "green", "red"]])
+    @pytest.mark.parametrize("marker_size", [1, 10])
+    @pytest.mark.parametrize("marker_opacity", [0.1, 0.5])
+    @pytest.mark.parametrize("show_cell", [True, False])
+    @pytest.mark.parametrize("width", [500, 1000])
+    @pytest.mark.parametrize("height", [500, 1000])
+    @pytest.mark.parametrize("title", [None, "title"])
+    @pytest.mark.parametrize("legend", [True, False])
+    def test_magnetic_entities(
+        self,
+        path,
+        tags,
+        colors,
+        marker_size,
+        marker_opacity,
+        show_cell,
+        width,
+        height,
+        title,
+        legend,
+    ):
+        setup = load_Builder(path)
+        fig = plot_magnetic_entities(
+            setup.magnetic_entities[:3],
+            tags=tags,
+            colors=colors,
+            marker_size=marker_size,
+            marker_opacity=marker_opacity,
+            show_cell=show_cell,
+            width=width,
+            height=height,
+            title=title,
+            legend=legend,
+        )
+
         assert isinstance(fig, go.Figure)
-        fig = plot_contour(setup)
+        fig = plot_magnetic_entities(
+            setup,
+            marker_size=marker_size,
+            marker_opacity=marker_opacity,
+            width=width,
+            height=height,
+            title=title,
+            legend=legend,
+        )
         assert isinstance(fig, go.Figure)
 
     @pytest.mark.parametrize(
@@ -49,39 +173,30 @@ class TestPlots:
             "./benchmarks/test_builder_2.pkl",
         ],
     )
-    def test_kspace(self, path):
-        setup = grogupy.load(path)
-        fig = plot_kspace(setup.kspace)
+    @pytest.mark.parametrize("colorscale", ["Viridis", "Plasma"])
+    @pytest.mark.parametrize("show_cell", [True, False])
+    @pytest.mark.parametrize("width", [500, 1000])
+    @pytest.mark.parametrize("height", [500, 1000])
+    @pytest.mark.parametrize("title", [None, "title"])
+    def test_onsite_anisotropy(self, path, colorscale, show_cell, width, height, title):
+        setup = load_Builder(path)
+        fig = plot_onsite_anisotropy(
+            setup.magnetic_entities,
+            colorscale=colorscale,
+            show_cell=show_cell,
+            width=width,
+            height=height,
+            title=title,
+        )
         assert isinstance(fig, go.Figure)
-        fig = plot_kspace(setup)
-        assert isinstance(fig, go.Figure)
-
-    @pytest.mark.parametrize(
-        "path",
-        [
-            "./benchmarks/test_builder.pkl",
-            "./benchmarks/test_builder_2.pkl",
-        ],
-    )
-    def test_magnetic_entities(self, path):
-        setup = grogupy.load(path)
-        fig = plot_magnetic_entities(setup.magnetic_entities)
-        assert isinstance(fig, go.Figure)
-        fig = plot_magnetic_entities(setup)
-        assert isinstance(fig, go.Figure)
-
-    @pytest.mark.parametrize(
-        "path",
-        [
-            "./benchmarks/test_builder.pkl",
-            "./benchmarks/test_builder_2.pkl",
-        ],
-    )
-    def test_onsite_anisotropy(self, path):
-        setup = grogupy.load(path)
-        fig = plot_onsite_anisotropy(setup.magnetic_entities)
-        assert isinstance(fig, go.Figure)
-        fig = plot_onsite_anisotropy(setup)
+        fig = plot_onsite_anisotropy(
+            setup,
+            colorscale=colorscale,
+            show_cell=show_cell,
+            width=width,
+            height=height,
+            title=title,
+        )
         assert isinstance(fig, go.Figure)
 
     @pytest.mark.parametrize(
@@ -91,13 +206,59 @@ class TestPlots:
             "./benchmarks/test_builder_2.pkl",
         ],
     )
+    @pytest.mark.parametrize("group", [True, False])
     @pytest.mark.parametrize("connect", [True, False])
-    @pytest.mark.parametrize("cell", [True, False])
-    def test_pairs(self, setup, path, connect, cell):
-        setup = grogupy.load(path)
-        fig = plot_pairs(setup.pairs, connect, cell)
+    @pytest.mark.parametrize("tags", [None, ["a"]])
+    @pytest.mark.parametrize("colors", [None, ["blue", "green"]])
+    @pytest.mark.parametrize("marker_size", [1, 10])
+    @pytest.mark.parametrize("marker_opacity", [0.1, 0.5])
+    @pytest.mark.parametrize("show_cell", [True, False])
+    @pytest.mark.parametrize("width", [500, 1000])
+    @pytest.mark.parametrize("height", [500, 1000])
+    @pytest.mark.parametrize("title", [None, "title"])
+    @pytest.mark.parametrize("legend", [True, False])
+    def test_pairs(
+        self,
+        path,
+        group,
+        connect,
+        tags,
+        colors,
+        marker_size,
+        marker_opacity,
+        show_cell,
+        width,
+        height,
+        title,
+        legend,
+    ):
+        setup = load_Builder(path)
+        fig = plot_pairs(
+            setup.pairs[:3],
+            group=group,
+            connect=connect,
+            tags=tags,
+            colors=colors,
+            marker_size=marker_size,
+            marker_opacity=marker_opacity,
+            show_cell=show_cell,
+            width=width,
+            height=height,
+            title=title,
+            legend=legend,
+        )
         assert isinstance(fig, go.Figure)
-        fig = plot_pairs(setup, connect, cell)
+        fig = plot_pairs(
+            setup,
+            group=group,
+            marker_size=marker_size,
+            marker_opacity=marker_opacity,
+            show_cell=show_cell,
+            width=width,
+            height=height,
+            title=title,
+            legend=legend,
+        )
         assert isinstance(fig, go.Figure)
 
     @pytest.mark.parametrize(
@@ -107,12 +268,56 @@ class TestPlots:
             "./benchmarks/test_builder_2.pkl",
         ],
     )
+    @pytest.mark.parametrize("heatplot", [True, False])
     @pytest.mark.parametrize("rescale", [-1, 0, 0.1, 1])
-    def test_DMI(self, path, rescale):
-        setup = grogupy.load(path)
-        fig = plot_DMI(setup.pairs, rescale)
+    @pytest.mark.parametrize("tags", [None, ["a", "b", "c"]])
+    @pytest.mark.parametrize("colors", [None, ["blue", "green", "red"]])
+    @pytest.mark.parametrize("colorscale", ["Viridis", "Plasma"])
+    @pytest.mark.parametrize("show_cell", [True, False])
+    @pytest.mark.parametrize("width", [500, 1000])
+    @pytest.mark.parametrize("height", [500, 1000])
+    @pytest.mark.parametrize("title", [None, "title"])
+    @pytest.mark.parametrize("legend", [True, False])
+    def test_DMI(
+        self,
+        path,
+        heatplot,
+        rescale,
+        tags,
+        colors,
+        colorscale,
+        show_cell,
+        width,
+        height,
+        title,
+        legend,
+    ):
+        setup = load_Builder(path)
+        fig = plot_DMI(
+            setup.pairs[:3],
+            heatplot=heatplot,
+            rescale=rescale,
+            tags=tags,
+            colors=colors,
+            colorscale=colorscale,
+            show_cell=show_cell,
+            width=width,
+            height=height,
+            title=title,
+            legend=legend,
+        )
         assert isinstance(fig, go.Figure)
-        fig = plot_DMI(setup, rescale)
+        fig = plot_DMI(
+            setup,
+            heatplot=heatplot,
+            rescale=rescale,
+            colorscale=colorscale,
+            show_cell=show_cell,
+            width=width,
+            height=height,
+            title=title,
+            legend=legend,
+        )
         assert isinstance(fig, go.Figure)
 
     @pytest.mark.parametrize(
@@ -123,11 +328,52 @@ class TestPlots:
         ],
     )
     @pytest.mark.parametrize("group", [True, False])
-    def test_Jiso_distance(self, path, group):
-        setup = grogupy.load(path)
-        fig = plot_Jiso_distance(setup.pairs, group)
+    @pytest.mark.parametrize("tags", [None, ["a", "b", "c"]])
+    @pytest.mark.parametrize("colors", [None, ["blue", "green", "red"]])
+    @pytest.mark.parametrize("marker_size", [1, 10])
+    @pytest.mark.parametrize("marker_opacity", [0.1, 0.5])
+    @pytest.mark.parametrize("width", [500, 1000])
+    @pytest.mark.parametrize("height", [500, 1000])
+    @pytest.mark.parametrize("title", [None, "title"])
+    @pytest.mark.parametrize("legend", [True, False])
+    def test_Jiso_distance(
+        self,
+        path,
+        group,
+        tags,
+        colors,
+        marker_size,
+        marker_opacity,
+        width,
+        height,
+        title,
+        legend,
+    ):
+        setup = load_Builder(path)
+        fig = plot_Jiso_distance(
+            setup.pairs[:3],
+            group=group,
+            tags=tags,
+            colors=colors,
+            marker_size=marker_size,
+            marker_opacity=marker_opacity,
+            width=width,
+            height=height,
+            title=title,
+            legend=legend,
+        )
+
         assert isinstance(fig, go.Figure)
-        fig = plot_Jiso_distance(setup, group)
+        fig = plot_Jiso_distance(
+            setup,
+            group=group,
+            marker_size=marker_size,
+            marker_opacity=marker_opacity,
+            width=width,
+            height=height,
+            title=title,
+            legend=legend,
+        )
         assert isinstance(fig, go.Figure)
 
     @pytest.mark.parametrize(
@@ -137,13 +383,56 @@ class TestPlots:
             "./benchmarks/test_builder_2.pkl",
         ],
     )
+    @pytest.mark.parametrize("group", [True, False])
     @pytest.mark.parametrize("normalise", [True, False])
-    @pytest.mark.parametrize("group", [True, False])
-    def test_DM_distance(self, normalise, group):
-        setup = grogupy.load(path)
-        fig = plot_DM_distance(setup.pairs, normalise, group)
+    @pytest.mark.parametrize("tags", [None, ["a", "b", "c"]])
+    @pytest.mark.parametrize("colors", [None, ["blue", "green", "red"]])
+    @pytest.mark.parametrize("marker_size", [1, 10])
+    @pytest.mark.parametrize("marker_opacity", [0.1, 0.5])
+    @pytest.mark.parametrize("width", [500, 1000])
+    @pytest.mark.parametrize("height", [500, 1000])
+    @pytest.mark.parametrize("title", [None, "title"])
+    @pytest.mark.parametrize("legend", [True, False])
+    def test_DM_distance(
+        self,
+        path,
+        group,
+        normalise,
+        tags,
+        colors,
+        marker_size,
+        marker_opacity,
+        width,
+        height,
+        title,
+        legend,
+    ):
+        setup = load_Builder(path)
+        fig = plot_DM_distance(
+            setup.pairs[:3],
+            group=group,
+            normalise=normalise,
+            tags=tags,
+            colors=colors,
+            marker_size=marker_size,
+            marker_opacity=marker_opacity,
+            width=width,
+            height=height,
+            title=title,
+            legend=legend,
+        )
         assert isinstance(fig, go.Figure)
-        fig = plot_DM_distance(setup, normalise, group)
+        fig = plot_DM_distance(
+            setup,
+            group=group,
+            normalise=normalise,
+            marker_size=marker_size,
+            marker_opacity=marker_opacity,
+            width=width,
+            height=height,
+            title=title,
+            legend=legend,
+        )
         assert isinstance(fig, go.Figure)
 
     @pytest.mark.parametrize(
@@ -154,28 +443,81 @@ class TestPlots:
         ],
     )
     @pytest.mark.parametrize("group", [True, False])
-    def test_J_S_distance(self, path, group):
-        setup = grogupy.load(path)
-        fig = plot_J_S_distance(setup.pairs, group)
+    @pytest.mark.parametrize("tags", [None, ["a", "b", "c"]])
+    @pytest.mark.parametrize("colors", [None, ["blue", "green", "red"]])
+    @pytest.mark.parametrize("marker_size", [1, 10])
+    @pytest.mark.parametrize("marker_opacity", [0.1, 0.5])
+    @pytest.mark.parametrize("width", [500, 1000])
+    @pytest.mark.parametrize("height", [500, 1000])
+    @pytest.mark.parametrize("title", [None, "title"])
+    @pytest.mark.parametrize("legend", [True, False])
+    def test_J_S_distance(
+        self,
+        path,
+        group,
+        tags,
+        colors,
+        marker_size,
+        marker_opacity,
+        width,
+        height,
+        title,
+        legend,
+    ):
+        setup = load_Builder(path)
+        fig = plot_J_S_distance(
+            setup.pairs[:3],
+            group=group,
+            tags=tags,
+            colors=colors,
+            marker_size=marker_size,
+            marker_opacity=marker_opacity,
+            width=width,
+            height=height,
+            title=title,
+            legend=legend,
+        )
         assert isinstance(fig, go.Figure)
-        fig = plot_J_S_distance(setup, group)
+        fig = plot_J_S_distance(
+            setup,
+            group=group,
+            marker_size=marker_size,
+            marker_opacity=marker_opacity,
+            width=width,
+            height=height,
+            title=title,
+            legend=legend,
+        )
         assert isinstance(fig, go.Figure)
 
-    @pytest.mark.xfail(raises=NotImplementedError)
-    @pytest.mark.parametrize(
-        "files",
-        [
-            [
-                "CrI3/CrI3_kset_1_1_1_eset_100_generalised-grogu.pkl",
-                "CrI3/CrI3_kset_3_3_1_eset_100_generalised-grogu.pkl",
-                "CrI3/CrI3_kset_5_5_1_eset_100_generalised-grogu.pkl",
-            ]
-        ],
-    )
-    @pytest.mark.parametrize("atol", [1e-5, 1])
-    @pytest.mark.parametrize("rtol", [1e-5, 1])
-    def test_1D_convergence(self, files, parameter, maxdiff, method):
-        fig = plot_1D_convergence(files, parameter, maxdiff, method)
+    @pytest.mark.parametrize("atol", [1e-5, 1, 10])
+    @pytest.mark.parametrize("rtol", [1e-5, 1, 10])
+    @pytest.mark.parametrize("marker_size", [1, 10])
+    @pytest.mark.parametrize("marker_opacity", [0.1, 0.5])
+    @pytest.mark.parametrize("width", [500, 1000])
+    @pytest.mark.parametrize("height", [500, 1000])
+    @pytest.mark.parametrize("title", [None, "title"])
+    def test_1D_convergence(
+        self,
+        atol,
+        rtol,
+        marker_size,
+        marker_opacity,
+        width,
+        height,
+        title,
+    ):
+        fig = plot_1D_convergence(
+            files=["./benchmarks/plot1.pkl", "./benchmarks/plot2.pkl"],
+            atol=atol,
+            rtol=rtol,
+            marker_size=marker_size,
+            marker_opacity=marker_opacity,
+            width=width,
+            height=height,
+            title=title,
+        )
+
         assert isinstance(fig, go.Figure)
 
 
